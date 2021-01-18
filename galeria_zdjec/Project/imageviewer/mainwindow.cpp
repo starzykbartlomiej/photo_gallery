60e8e364826aa00ace1e1fa35dd8ca7667bd3149
starzyk_bartlomiej <starzyk.bartlomiej@gmail.com>
2021-01-18 20:42:47 +0100

    Test image viewing

--- galeria_zdjec/Project/imageviewer/mainwindow.cpp
+++ galeria_zdjec/Project/imageviewer/mainwindow.cpp
@@ -1,11 +1,16 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include <QPixmap>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QPixmap pix("/home/student/oop_2020_galeria_zdjec/galeria_zdjec/Project/photos/grandma.jpg");
+    int width = ui->label_pic->width();
+    int height = ui->label_pic->height();
+    ui->label_pic->setAlignment(Qt::AlignCenter);
+    ui->label_pic->setPixmap(pix.scaled(width,height, Qt::KeepAspectRatio));
 }
 
 MainWindow::~MainWindow()
