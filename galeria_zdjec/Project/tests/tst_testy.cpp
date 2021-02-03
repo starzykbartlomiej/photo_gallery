b196c369946166c0da8e748ccce422c8d089d1f9
Aleksander Wąs <alwas@student.agh.edu.pl>
2021-02-03 23:10:02 +0100

    ex_test_execution

--- galeria_zdjec/Project/tests/tst_testy.cpp
+++ galeria_zdjec/Project/tests/tst_testy.cpp
@@ -7,29 +7,13 @@ class Testy : public QObject
 {
     Q_OBJECT
 
-public:
-    Testy();
-    ~Testy();
-
 private slots:
-    void test_case1();
+    void add(){
+        QCOMPARE(3, 2+1);
+    }
 
 };
 
-Testy::Testy()
-{
-
-}
-
-Testy::~Testy()
-{
-
-}
-
-void Testy::test_case1()
-{
-
-}
 
 QTEST_APPLESS_MAIN(Testy)
 
